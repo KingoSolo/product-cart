8a8a913aee443f303d35691ab2af3b2252825c7b
Solomon Adeniran <soluwaseun806@gmail.com>
2025-12-29 01:40:35 +0100

    add template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar.component';
 import { ProductCard } from './product-card/product-card.component';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, ProductCard],
+  imports: [CommonModule,RouterOutlet, Navbar, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('product-cart');
+ products: Product[] = [{ id: 1, name: 'Wireless Headphones', description: 'Noise-cancelling', price: 199.99, imageUrl: 'https://via.placeholder.com/200' },
+  // add 5
+  ];           
+  filteredProducts = this.products;
+  cart: Product[] = [];
+
+  onSearch(query: string) {           // Add this method
+    if (!query) {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(p =>
+        p.name.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+  }
+
+  onProductSelected(product: Product) { // Add this
+    if (!this.cart.some(item => item.id === product.id)) {
+      this.cart.push(product);
+    }
+  }
+
+  isSelected(product: Product): boolean { // Add this
+    return this.cart.some(item => item.id === product.id);
+  }
 }
