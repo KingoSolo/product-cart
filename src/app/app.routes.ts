b0df393419e7133aa97aa9fe38f30126cfa5006e
Solomon Adeniran <soluwaseun806@gmail.com>
2026-01-27 14:57:41 +0100

    add product service and sample data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+
+export const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }
+];
